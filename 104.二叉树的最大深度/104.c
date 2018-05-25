f8bfc793c3e4a90bdca9442bae48970fd21d342a
Er1cZ <39368247+Er1cZ@users.noreply.github.com>
2018-05-25 15:45:07 +0800

    Add files via upload

---
+++ 104.二叉树的最大深度/104.c
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    if(root == NULL)
+        return 0;
+    else
+    {
+        int leftMax =  maxDepth(root->left);
+        int rightMax = maxDepth(root->right);
+        
+        return leftMax > rightMax ? (leftMax + 1) : (rightMax + 1);
+    }
+}
