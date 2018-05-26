9cbae670f03d42ce01a32b674df9d11374ca1b83
Er1cZ <39368247+Er1cZ@users.noreply.github.com>
2018-05-26 20:55:40 +0800

    Add files via upload

---
+++ 190.颠倒二进制位/190.c
@@ -0,0 +1,11 @@
+uint32_t reverseBits(uint32_t n) {
+    int result = 0;
+    
+    for(int i = 0; i < 32; i++)
+    {
+        result = (result << 1) | (n & 1);
+        n = n >> 1;
+    }
+    
+    return (uint32_t)result;
+}
